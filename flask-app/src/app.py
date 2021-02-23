18313fda2566c651c76ed90a95f305618cc8ff1a
Ignacio Millan <ignacio@awardforce.com>
2021-02-23 16:37:41 +0100

    Add flask app

---
+++ flask-app/src/app.py
@@ -0,0 +1,23 @@
+# flask_web/app.py
+import sys
+import os
+from flask import Flask
+
+import config as cfg
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+
+    # Leer variables de entorno
+    foo = os.getenv('FOO', "BAR")
+
+    # Leer archivo configuracion
+    password = cfg.password
+
+    return f'''Hey, we have Flask in a Docker container!!!! 
+    <br> envvar: {foo} 
+    <br> password: {password}'''
+
+app.run(debug=True, host='0.0.0.0')
